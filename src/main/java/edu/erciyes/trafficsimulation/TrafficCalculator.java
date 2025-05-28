155d66412fae622498a2ad41695a381d69b0c3ac
ergul13 <152935337+ergul13@users.noreply.github.com>
2025-05-28 20:15:50 +0300

    Update TrafficCalculator.java
    süreler doğru hesaplanıyor

--- src/main/java/edu/erciyes/trafficsimulation/TrafficCalculator.java
+++ src/main/java/edu/erciyes/trafficsimulation/TrafficCalculator.java
@@ -2,73 +2,104 @@ package edu.erciyes.trafficsimulation;
 
 public class TrafficCalculator {
 
-    private static int overplusNumber = 0;
+    public void calculateTraffic(TrafficLight northLight, int northV,
+                                 TrafficLight eastLight, int eastV,
+                                 TrafficLight southLight, int southV,
+                                 TrafficLight westLight, int westV) {
 
-    public void calculateTraffic(TrafficLight northLight, int northV, TrafficLight eastLight, int eastV, TrafficLight southLight, int southV, TrafficLight westLight, int westV) {
-        int totalCar = northV + eastV + southV + westV;
-        int timePerCar = 0;
-        if (totalCar != 0) timePerCar = TrafficLight.CYCLE_TIME / totalCar;
+        final int YELLOW_TIME = 2;
+        final int TOTAL_CYCLE = TrafficLight.CYCLE_TIME;
+        final int TOTAL_GREEN = TOTAL_CYCLE - 4 * YELLOW_TIME; // Artık toplam yeşil süre = 112
 
-        int northGreenTime = northV * timePerCar - 3;
-        int eastGreenTime = eastV * timePerCar - 3;
-        int southGreenTime = southV * timePerCar - 3;
-        int westGreenTime = westV * timePerCar - 3;
-
-        System.out.println("north green time : " + northGreenTime);
-        System.out.println("east green time : " + eastGreenTime);
-        System.out.println("south green time : " + southGreenTime);
-        System.out.println("west green time : " + westGreenTime);
-
-        boolean isOverplusNorth = isOverplus(northGreenTime);
-        boolean isOverplusEast = isOverplus(eastGreenTime);
-        boolean isOverplusSouth = isOverplus(southGreenTime);
-        boolean isOverplusWest = isOverplus(westGreenTime);
-
-        northGreenTime = checkTime(northGreenTime);
-        eastGreenTime = checkTime(eastGreenTime);
-        southGreenTime = checkTime(southGreenTime);
-        westGreenTime = checkTime(westGreenTime);
-
-        int totalTime = northGreenTime + eastGreenTime + southGreenTime + westGreenTime;
-        int overplusPerLight = 0;
-        if (overplusNumber != 0) overplusPerLight = (TrafficLight.CYCLE_TIME - totalTime) / overplusNumber;
-
-        if (!isOverplusNorth) northGreenTime += overplusPerLight;
-        if (!isOverplusEast) eastGreenTime += overplusPerLight;
-        if (!isOverplusSouth) southGreenTime += overplusPerLight;
-        if (!isOverplusWest) westGreenTime += overplusPerLight;
-
-        northLight.setTimes(northGreenTime);
-        eastLight.setTimes(eastGreenTime);
-        southLight.setTimes(southGreenTime);
-        westLight.setTimes(westGreenTime);
-
-        System.out.println("north green time : " + northGreenTime);
-        System.out.println("east green time : " + eastGreenTime);
-        System.out.println("south green time : " + southGreenTime);
-        System.out.println("west green time : " + westGreenTime);
-
-        overplusNumber = 0;
-    }
-
-    private static int checkTime(int time) {
-        int max = TrafficLight.MAX_GREEN_TIME;
-        int min = TrafficLight.MIN_GREEN_TIME;
-        if (time >= max) {
-            time = max;
-        } else if (time <= min) {
-            time = min;
+        int totalCars = northV + eastV + southV + westV;
+        if (totalCars == 0) {
+            // araç yoksa herkes minimum
+            northLight.setTimes(TrafficLight.MIN_GREEN_TIME);
+            eastLight.setTimes(TrafficLight.MIN_GREEN_TIME);
+            southLight.setTimes(TrafficLight.MIN_GREEN_TIME);
+            westLight.setTimes(TrafficLight.MIN_GREEN_TIME);
+            return;
         }
-        return time;
-    }
 
-    private static boolean isOverplus(int time) {
-        int max = TrafficLight.MAX_GREEN_TIME;
-        int min = TrafficLight.MIN_GREEN_TIME;
-        if (time > max || time < min) {
-            overplusNumber++;
-            return true;
+        // 1. Oransal ilk süreleri hesapla
+        double northRatio = (double) northV / totalCars;
+        double eastRatio  = (double) eastV  / totalCars;
+        double southRatio = (double) southV / totalCars;
+        double westRatio  = (double) westV  / totalCars;
+
+        int northG = (int) Math.round(northRatio * TOTAL_GREEN);
+        int eastG  = (int) Math.round(eastRatio  * TOTAL_GREEN);
+        int southG = (int) Math.round(southRatio * TOTAL_GREEN);
+        int westG  = (int) Math.round(westRatio  * TOTAL_GREEN);
+
+        // 2. Limit dışı süreleri sabitle
+        boolean fixNorth = false, fixEast = false, fixSouth = false, fixWest = false;
+        if (northG < TrafficLight.MIN_GREEN_TIME) {
+            northG = TrafficLight.MIN_GREEN_TIME;
+            fixNorth = true;
+        } else if (northG > TrafficLight.MAX_GREEN_TIME) {
+            northG = TrafficLight.MAX_GREEN_TIME;
+            fixNorth = true;
         }
-        return false;
+
+        if (eastG < TrafficLight.MIN_GREEN_TIME) {
+            eastG = TrafficLight.MIN_GREEN_TIME;
+            fixEast = true;
+        } else if (eastG > TrafficLight.MAX_GREEN_TIME) {
+            eastG = TrafficLight.MAX_GREEN_TIME;
+            fixEast = true;
+        }
+
+        if (southG < TrafficLight.MIN_GREEN_TIME) {
+            southG = TrafficLight.MIN_GREEN_TIME;
+            fixSouth = true;
+        } else if (southG > TrafficLight.MAX_GREEN_TIME) {
+            southG = TrafficLight.MAX_GREEN_TIME;
+            fixSouth = true;
+        }
+
+        if (westG < TrafficLight.MIN_GREEN_TIME) {
+            westG = TrafficLight.MIN_GREEN_TIME;
+            fixWest = true;
+        } else if (westG > TrafficLight.MAX_GREEN_TIME) {
+            westG = TrafficLight.MAX_GREEN_TIME;
+            fixWest = true;
+        }
+
+        // 3. Sabitlenmiş süreleri ve araçları topla
+        int fixedTotal = 0;
+        int fixedCars = 0;
+        if (fixNorth) { fixedTotal += northG; fixedCars += northV; }
+        if (fixEast)  { fixedTotal += eastG;  fixedCars += eastV; }
+        if (fixSouth) { fixedTotal += southG; fixedCars += southV; }
+        if (fixWest)  { fixedTotal += westG;  fixedCars += westV; }
+
+        // 4. Kalan süreyi paylaştırılabilir yönlere oranlı olarak dağıt
+        int remaining = TOTAL_GREEN - fixedTotal;
+        int adjustableCars = totalCars - fixedCars;
+
+        if (!fixNorth && adjustableCars > 0)
+            northG = (int) Math.round((double) northV / adjustableCars * remaining);
+        if (!fixEast && adjustableCars > 0)
+            eastG = (int) Math.round((double) eastV / adjustableCars * remaining);
+        if (!fixSouth && adjustableCars > 0)
+            southG = (int) Math.round((double) southV / adjustableCars * remaining);
+        if (!fixWest && adjustableCars > 0)
+            westG = (int) Math.round((double) westV / adjustableCars * remaining);
+
+        // 5. Işık sürelerini ata
+        northLight.setTimes(northG);
+        eastLight.setTimes(eastG);
+        southLight.setTimes(southG);
+        westLight.setTimes(westG);
+
+        // 6. Debug
+        System.out.println("North Green : " + northG);
+        System.out.println("East  Green : " + eastG);
+        System.out.println("South Green : " + southG);
+        System.out.println("West  Green : " + westG);
+        System.out.println("Toplam Yeşil : " + (northG + eastG + southG + westG));
+        System.out.println("Toplam Sarı  : 8");
+        System.out.println("Toplam Süre  : " + (northG + eastG + southG + westG + 8));
     }
 }
